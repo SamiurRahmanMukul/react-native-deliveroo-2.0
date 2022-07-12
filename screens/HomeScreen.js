7d3ba2c7ada14a620761d7e686486f85da7f93b4
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2022-07-12 08:14:23 +0600

    react-native app navigation & tailwind-css setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import { Text, View } from "react-native";
+
+export class HomeScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text className="text-red-500 text-2xl text-center mt-5 font-bold">HomeScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default HomeScreen;
